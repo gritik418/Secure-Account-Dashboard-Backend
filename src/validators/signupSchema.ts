6e04e938d4fc4609eccf5c1dee7cb6151724b3d2
Ritik <160255700+gritik418@users.noreply.github.com>
2024-05-03 11:09:54 +0530

    Verification schema updated

--- src/validators/signupSchema.ts
+++ src/validators/signupSchema.ts
@@ -2,7 +2,7 @@ import vine from "@vinejs/vine";
 
 const signupSchema = vine.object({
   first_name: vine.string().minLength(3),
-  username: vine.string(),
+  username: vine.string().minLength(4),
   email: vine.string().email(),
   password: vine.string().minLength(8).maxLength(32).confirmed(),
 });
